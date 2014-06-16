2c4986706acaa96379dedd50302eb659d029cf0b
Felipe <fsr@FSR.local>
2014-06-16 20:50:13 -0300

    second commit

---
+++ module/Application/Module.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Log\Writer\FirePhp;
+use Zend\Log\Logger;
+
+require('vendor/firephp/firephp-core/lib/FirePHPCore/FirePHP.class.php');
+
+class Module {
+
+    public function onBootstrap(MvcEvent $e) {
+        $e->getApplication()->getServiceManager()->get('translator');
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'Zend\Log\FirePhp' => function($sm) {
+                    $writer_firebug = new \Zend\Log\Writer\FirePhp();
+                    $logger = new \Zend\Log\Logger();
+                    $logger->addWriter($writer_firebug);
+                    return $logger;
+                },
+            ),
+        );
+    }
+
+}
